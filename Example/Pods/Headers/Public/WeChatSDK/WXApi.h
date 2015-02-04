f00f7bae27c5a760e2aa71b591c2aacc65d64c6d
SongLi <chuangyi0128@gmail.com>
2015-02-04 21:48:52 +0800

    WeChatSDK 1.5

---
+++ Example/Pods/Headers/Public/WeChatSDK/WXApi.h
@@ -0,0 +1,1 @@
+../../../../../WeChatSDK/WXApi.h
---
+++ Example/Pods/Target Support Files/Pods-WeChatSDK/Pods-WeChatSDK-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// WeChatSDK
+#define COCOAPODS_POD_AVAILABLE_WeChatSDK
+#define COCOAPODS_VERSION_MAJOR_WeChatSDK 1
+#define COCOAPODS_VERSION_MINOR_WeChatSDK 5
+#define COCOAPODS_VERSION_PATCH_WeChatSDK 0
+
